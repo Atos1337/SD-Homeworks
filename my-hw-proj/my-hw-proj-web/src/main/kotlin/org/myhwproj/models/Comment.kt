34b2b5a8a5e3d1a18c8cc75bba9b6f2af8c10a7a
Atos1337 <victor.samoilov.2002@mail.ru>
2022-05-24 20:15:03 +0300

    Fix worker and comment length

--- my-hw-proj/my-hw-proj-web/src/main/kotlin/org/myhwproj/models/Comment.kt
+++ my-hw-proj/my-hw-proj-web/src/main/kotlin/org/myhwproj/models/Comment.kt
@@ -4,10 +4,11 @@ import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.Id
 import javax.persistence.ManyToOne
+import javax.persistence.Lob
 
 @Entity
 data class Comment(
     @ManyToOne var submission: Submission,
-    var content: String,
+    @Lob var content: String,
     @Id @GeneratedValue var id: Long? = null
 )
