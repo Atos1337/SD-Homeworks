8af37f53b24c8cb598dfdd330180901087634bea
Mikhail Lopatin <mixoz3101@gmail.com>
2022-05-26 21:11:29 +0200

    Fix

--- my-hw-proj/my-hw-proj-web/src/main/kotlin/org/myhwproj/HtmlController.kt
+++ my-hw-proj/my-hw-proj-web/src/main/kotlin/org/myhwproj/HtmlController.kt
@@ -8,7 +8,11 @@ import org.myhwproj.services.TeacherService
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.PostMapping
 
 @Controller
 class HtmlController(
