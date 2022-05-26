9ffdba1a583d091204b07b84f52703be9afd2767
Mikhail Lopatin <mixoz3101@gmail.com>
2022-05-26 21:02:27 +0200

    Fix

--- my-hw-proj/my-hw-proj-web/src/main/kotlin/org/myhwproj/repositories/HomeworkRepository.kt
+++ my-hw-proj/my-hw-proj-web/src/main/kotlin/org/myhwproj/repositories/HomeworkRepository.kt
@@ -2,8 +2,6 @@ package org.myhwproj.repositories
 
 import org.myhwproj.models.Homework
 import org.springframework.data.repository.CrudRepository
-import java.sql.Timestamp
-import java.util.Date
 
 interface HomeworkRepository : CrudRepository<Homework, Long> {
     fun findAllByDeadlineAfterAndPublicationDateBeforeOrderByDeadline(deadline: String, publicationDate: String): Iterable<Homework>
